1fc199f82083aa36eaa77e5813e8bc0701847906
carlossoler20 <78773255+carlossoler20@users.noreply.github.com>
2021-11-10 18:38:30 -0500

    final

--- login/main.js
+++ login/main.js
@@ -1,58 +1,67 @@
 $(document).ready(function () {
-    var correo = ["carlossoler17@gmail.com", "carlos.solers@cun.edu.co"];
-    var password = [123456, 654321];
-    var email;
-    var pass;
-    var a;
-    var b;
+////Lo comentado era otra logica que estaba haciendo////
+//     $("#btn-iniciar").click(function(){
+//         var correo = ["carlossoler17@gmail.com", "carlos.solers@cun.edu.co"];
+//         var password = [123456, 654321];
+//         var email;
+//         var pass;
+//         $("#error").hide();
+//         if((email != correo[0]) || (email != correo[1])) {
+//                     $("#error").show();
+//                     return true;
+//                 }
+//                 else {
+//                     alert("HOla")
+//                     return;
+//                 }    
 
+// })
+$("#registro").hide();
+$("#btn-crear").click(function(){
+    $("#registro").show();
+})
+var correo = ["carlossoler17@gmail.com", "carlos.solers@cun.edu.co"];
+var password = [123456, 654321];
+var email;
+var pass;
+var a;
+var b;
+var msgBienvenido;
     function validarCorreo() {
         email = $("#exampleInputEmail1").val();
         if ((email == correo[0]) || (email == correo[1])) {
-            alert("Es verdadero")
             a=true
             return a;
         }
         else {
-            alert("Es falso")
-            return false;
+            var x="Email Invalido";
+            $("#resultadoNegativo").text(x);
+            return;
         }
     }
     
     function validarPass() {
         pass = $("#exampleInputPassword1").val();
         if ((pass == password[0]) || (pass == password[1])) {
-            alert("Es verdadero")
             b=true;
             return b;
         }
         else {
-            alert("Es falso")
-            return false;
+            var y="Contraseña invalido"
+            $("#resultadoNegativo").text(y);
+            return ;
         }
     }
         function valido(){
         if(a==true && b==true){
-            alert("Bienvenido")
+            var x="Bienvenido";
+            $("#resultadoPositivo").text(x);
             return;
         }
         else{
-            alert("Correo o Contraseña incorrecta")
+            var y="Correo o contraseña invalido"
+            $("#resultadoNegativo").text(y);
             return;
         }
     }
-    $("#btn-inicio").click(function () {
-        validarCorreo();
-        validarPass();
-        valido();
-    })
-
-
-    // if((==correo[0]||==correo[1])(==pass[0]||==pass[1])){
-    //     Bienvenido
-    // }
-    // else{
-    //     Clave incorrecta
-    // }
-
 })
